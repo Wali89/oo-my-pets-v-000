0609a1ee808986a257c4e9967baec8e979d651cd
Wali Rahman <wali.rahman89@yahoo.com>
2018-01-03 01:44:21 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -69,7 +69,9 @@ class Owner
       end
       animals.clear
     end
-
   end
 
+  def list_pets
+     "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
+  end
 end
