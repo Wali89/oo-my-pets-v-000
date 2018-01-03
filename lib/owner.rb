ceebd35ff427bef292487cf35891daf88f228bef
Wali Rahman <wali.rahman89@yahoo.com>
2018-01-03 00:35:00 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,11 +2,13 @@ class Owner
   attr_reader
   attr_accessor
   @@all = []
+  @@counter = 0
 
   def initialize(name)
     @pets = {fishes: [], cats: [], dogs: []}
     @name = name
     @@all << self
+    @@counter += 1
   end
 
   def self.all
