5ab7af9bd9470bb1d1ffee8dbf5bdaefac2337e4
Wali Rahman <wali.rahman89@yahoo.com>
2018-01-03 01:42:44 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -63,6 +63,13 @@ class Owner
   end
 
   def sell_pets
+    pets.each do |species, animals|
+      animals.each do |animal|
+        animal.mood = "nervous"
+      end
+      animals.clear
+    end
 
   end
+
 end
